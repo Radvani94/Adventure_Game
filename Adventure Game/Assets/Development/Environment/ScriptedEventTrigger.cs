255300d05d3bc178c22efd59a72e12998ee3ed33
UpamanyuYavalkar <uyavalkar@gmail.com>
2020-05-24 22:09:28 +0530

    fun stuff

---
+++ Adventure Game/Assets/Development/Environment/ScriptedEventTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ScriptedEventTrigger : MonoBehaviour
+{
+    //private float TotalTime = 0;
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    //void waitFor(float _seconds)
+    //{
+    //    StartCoroutine("waitForMe", _seconds);
+    //}
+
+    //void DoThis()
+    //{
+    //    Debug.Log("DID THIS");
+    //}
+
+    //IEnumerator waitForMe(float secs)
+    //{
+
+    //    while (TotalTime<=secs)
+    //    {
+    //        TotalTime += Time.deltaTime;
+    //        Debug.Log("COUNTING");
+    //        yield return null;
+    //    }
+    //    Debug.Log("TIME DONE");
+    //    DoThis();
+    //    TotalTime = 0;
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    waitFor(5);
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+        
+    //}
+}
---
+++ Adventure Game/Assets/Development/Environment/ScriptedTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedTest : ScriptedEventTrigger
+{
+    private float TotalTime = 0;
+    public float Delay = 3;
+    public GameObject TestBlocks;
+    public Rigidbody[] bodiez;
+
+    private void Start()
+    {
+        bodiez = TestBlocks.GetComponentsInChildren<Rigidbody>();
+    }
+    void waitFor(float _seconds)
+    {
+        StartCoroutine("waitForMe", _seconds);
+    }
+
+    void DoThis()
+    {
+        Debug.Log("DID THIS");
+        for(int i =0; i<bodiez.Length; i++)
+        {
+            bodiez[i].useGravity = true;
+        }
+    }
+
+    IEnumerator waitForMe(float secs)
+    {
+
+        while (TotalTime <= secs)
+        {
+            TotalTime += Time.deltaTime;
+            Debug.Log("COUNTING");
+            yield return null;
+        }
+        Debug.Log("TIME DONE");
+        DoThis();
+        TotalTime = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        waitFor(Delay);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
